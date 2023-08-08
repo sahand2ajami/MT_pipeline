b20cf716fd7df146dd059c5abc8a2db145d3734e
anchit1729 <champu.1729@icloud.com>
2023-08-08 12:38:06 -0400

    My genius brain is writing poetry, not code

--- generate_plots.py
+++ generate_plots.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import argparse
+import pprint
 
 from openpyxl import load_workbook
 
@@ -55,7 +56,11 @@ def main(participant_id, experiment_block):
         for i in range(1, NUM_QUESTIONS_EMBODIMENT + 1):
             embodiment_responses[j][f'Q{i}'] = []
 
-    
+    # print(presence_responses)
+    # print(tlx_responses)
+    # print(embodiment_responses)    
+
+
     # now, regardless of how many participants we are plotting for,
     # iterate over the all_data object to accumulate scores
     count = 0
@@ -68,15 +73,82 @@ def main(participant_id, experiment_block):
         # iterate over experiment blocks to accumulate data
         for block_id in range(experiment_block):
             for question_id in range(1, NUM_QUESTIONS_PRESENCE + 1):
-                presence_responses[block_id][f'Q{question_id}'].append(df[1 + block_id][1 + question_id])
+                presence_responses[block_id][f'Q{question_id}'].append(df.iloc[1 + block_id, 1 + question_id])
             for question_id in range(1, NUM_QUESTIONS_TLX + 1):
-                tlx_responses[block_id][f'Q{question_id}'].append(df[5 + block_id][1 + question_id])
-            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT + 1):
-                
-                
-            
+                tlx_responses[block_id][f'Q{question_id}'].append(df.iloc[5 + block_id, 1 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + 1):
+                embodiment_responses[block_id][f'Q{question_id}'].append(df.iloc[9 + block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_AGENCY + 1):
+                embodiment_responses[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP}'].append(df.iloc[11 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_TACTILE + 1):
+                embodiment_responses[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY}'].append(df.iloc[13 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_LOCATION + 1):
+                embodiment_responses[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY + NUM_QUESTIONS_EMBODIMENT_TACTILE}'].append(df.iloc[15 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_APPEARANCE + 1):
+                embodiment_responses[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY + NUM_QUESTIONS_EMBODIMENT_TACTILE + NUM_QUESTIONS_EMBODIMENT_LOCATION}'].append(df.iloc[17 + 10 * block_id, 2 + question_id])
     
-    print(f'Plotting data for {count} participant(s) under WithHaptics.')
+    pp = pprint.PrettyPrinter(indent=3)
+
+    print(f'Found {count} participant(s) under WithHaptics.')
+    print(f'Presence:')
+    pp.pprint(presence_responses)
+    print(f'TLX:')
+    pp.pprint(tlx_responses)
+    print(f'Embodiment:')
+    pp.pprint(embodiment_responses)
+
+    # create dictionaries to store questions and their score lists for each questionnaire
+    presence_responses_no = [{}, {}, {}]
+    for j in range(experiment_block):
+        for i in range(1, NUM_QUESTIONS_PRESENCE + 1):
+            presence_responses_no[j][f'Q{i}'] = []
+    
+    tlx_responses_no = [{}, {}, {}]
+    for j in range(experiment_block):
+        for i in range(1, NUM_QUESTIONS_TLX + 1):
+            tlx_responses_no[j][f'Q{i}'] = []
+    
+    embodiment_responses_no = [{}, {}, {}]
+    for j in range(experiment_block):
+        for i in range(1, NUM_QUESTIONS_EMBODIMENT + 1):
+            embodiment_responses_no[j][f'Q{i}'] = []
+
+
+    # now, regardless of how many participants we are plotting for,
+    # iterate over the all_data object to accumulate scores
+    count = 0
+    # 1: WithHaptics participants
+    for sheet_name, df in all_data.items():
+        # check if the sheet is for a WithHaptics participant
+        if 'WithoutHaptics' not in sheet_name:
+            continue
+        count += 1
+        # iterate over experiment blocks to accumulate data
+        for block_id in range(experiment_block):
+            for question_id in range(1, NUM_QUESTIONS_PRESENCE + 1):
+                presence_responses_no[block_id][f'Q{question_id}'].append(df.iloc[1 + block_id, 1 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_TLX + 1):
+                tlx_responses_no[block_id][f'Q{question_id}'].append(df.iloc[5 + block_id, 1 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + 1):
+                embodiment_responses_no[block_id][f'Q{question_id}'].append(df.iloc[9 + block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_AGENCY + 1):
+                embodiment_responses_no[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP}'].append(df.iloc[11 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_TACTILE + 1):
+                embodiment_responses_no[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY}'].append(df.iloc[13 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_LOCATION + 1):
+                embodiment_responses_no[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY + NUM_QUESTIONS_EMBODIMENT_TACTILE}'].append(df.iloc[15 + 10 * block_id, 2 + question_id])
+            for question_id in range(1, NUM_QUESTIONS_EMBODIMENT_APPEARANCE + 1):
+                embodiment_responses_no[block_id][f'Q{question_id + NUM_QUESTIONS_EMBODIMENT_OWNERSHIP + NUM_QUESTIONS_EMBODIMENT_AGENCY + NUM_QUESTIONS_EMBODIMENT_TACTILE + NUM_QUESTIONS_EMBODIMENT_LOCATION}'].append(df.iloc[17 + 10 * block_id, 2 + question_id])
+    
+    pp = pprint.PrettyPrinter(indent=3)
+
+    print(f'Found {count} participant(s) under WithoutHaptics.')
+    print(f'Presence:')
+    pp.pprint(presence_responses_no)
+    print(f'TLX:')
+    pp.pprint(tlx_responses_no)
+    print(f'Embodiment:')
+    pp.pprint(embodiment_responses_no)
     
 
 
