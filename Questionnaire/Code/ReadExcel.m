34bbe655fb387b14240574dc1d60127b81b0ff16
Sahand Ajami <sahand.ajami@gmail.com>
2023-08-28 20:14:24 -0400

    NASA-TLX is added. It's plot needs to be clean for the paper

--- Questionnaire/Code/ReadExcel.m
+++ Questionnaire/Code/ReadExcel.m
@@ -1,5 +1,6 @@
 function myStruct = ReadExcel(myStruct, excelFilePath, cellRange, QuestionnaireString, CondtionString, sheetName)
-    data = xlsread(excelFilePath, sheetName, cellRange);
+%     data = xlsread(excelFilePath, sheetName, cellRange);
+    data = readmatrix(excelFilePath, 'Sheet', sheetName, 'Range', cellRange);
     myStruct.(sheetName(4:end)).(QuestionnaireString).(CondtionString).(strcat('S', sheetName(1:2))) = data;
     
 end
